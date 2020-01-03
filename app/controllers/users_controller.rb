d9d37cd6b048418981d295ca174b81d42f9eb662
alaatarek95 <alaatarek95@gmail.com>
2020-01-03 13:48:12 +0200

    add devise views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
     def index 
-        if current_user.role == "driver" 
-            redirect_to drive_path
-        elsif current_user.role == "passenger"
-            redirect_to passenger_path
-        elsif current_user.role == "admin" 
-            redirect_to admin_path
+        if current_user
+            if current_user.role == "driver" 
+                redirect_to drive_path
+            elsif current_user.role == "passenger"
+                redirect_to passenger_path
+            elsif current_user.role == "admin" 
+                redirect_to admin_path
+            end
         else
             redirect_to new_user_session_path
         end
