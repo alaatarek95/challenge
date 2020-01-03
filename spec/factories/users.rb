7f6bc10e83fbd6182112089211d6ead79384fa4b
alaatarek95 <alaatarek95@gmail.com>
2020-01-04 01:35:58 +0200

    add trips feature tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,14 +7,6 @@ FactoryBot.define do
         phone { "12345678891" }
         role { 1 }
     end
-    factory :admin, class: "User" do
-        full_name { "admin alaa" }
-        email  { "admin@test.com" }
-        password { "123456" }
-        password_confirmation { "123456" }
-        phone { "12345678895" }
-        role { 2 }
-    end
     factory :driver, class: "User" do
         full_name { "driver alaa" }
         email  { "driver@test.com" }
--- spec/features/trip_spec.rb
+++ spec/features/trip_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 
-RSpec.feature "Pickup", js: true, driver: :selenium_chrome, type: :feature do
-	feature 'add new pickup' do
-		scenario 'pickup faild to be added' do
-			login_as(FactoryBot.create(:passenger))
+RSpec.feature "Trip", js: true, driver: :selenium_chrome, type: :feature do
+	feature 'add new trip' do
+		scenario 'trip faild to be added' do
+			login_as(FactoryBot.create(:driver))
 			FactoryBot.create(:place_one)
 			FactoryBot.create(:place_two)
-			visit new_pickup_path
-			within '#new_pickup' do 
+			visit new_trip_path
+			within '#new_trip' do 
 			
-				fill_in id: 'pickup_dep_time',  with: DateTime.now.yesterday.strftime("%m%d%Y\t%I%M%P")
-				select('test', :from => 'pickup_destination_id')
-				select('test 2', :from => 'pickup_source_id')
-				click_button 'Create Pickup'
+				fill_in id: 'trip_dep_time',  with: DateTime.now.yesterday.strftime("%m%d%Y\t%I%M%P")
+				fill_in id: 'trip_seats', with: 2
+				select('test', :from => 'trip_destination_id')
+				select('test 2', :from => 'trip_source_id')
+				click_button 'Create Trip'
 			end
-			current_path.should == pickups_path
+			current_path.should == trips_path
 			page.should have_selector('#error_explanation')
 
 
