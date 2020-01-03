ec468dee643b2c019fc5d9346251bac1ec74c33f
alaatarek95 <alaatarek95@gmail.com>
2020-01-03 14:43:39 +0200

    fix sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
     layout 'application'
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def after_sign_in_path_for(resource)
         if current_user.role == "driver" 
             driver_path
@@ -13,5 +17,11 @@ class ApplicationController < ActionController::Base
     end
     def after_sign_out_path_for(resource)
         new_user_session_path
+    end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name,:role, :phone])
     end
 end
